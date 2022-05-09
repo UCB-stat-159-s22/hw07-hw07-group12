74195498e7dd0642ad3cedf7198f32a4fa2b4877
Ryan Keivanfar <nikeivanfar@gmail.com>
2022-05-09 22:24:03 +0000

    create tests directory; create test for generating simulated dataset: highly variable genes

---
+++ tests/test_simulated.py
@@ -0,0 +1,20 @@
+import scvelo as scv
+import numpy as np
+
+def test_high_var_subset():
+    adata = scv.datasets.simulation(random_seed=1, n_vars=8)
+    bdata = adata.copy()
+    scv.pp.filter_and_normalize(adata, n_top_genes=5, subset_highly_variable=True)
+    scv.pp.filter_and_normalize(bdata, n_top_genes=5, subset_highly_variable=False)
+    scv.pp.pca(adata)
+    scv.pp.pca(bdata)
+    scv.pp.moments(adata, use_rep="pca")
+    scv.pp.moments(bdata, use_rep="pca")
+    scv.tl.velocity_graph(adata)
+    scv.tl.velocity_graph(bdata)
+    bdata._inplace_subset_var(bdata.var["highly_variable"])
+    assert np.allclose(adata.layers["Ms"][0], bdata.layers["Ms"][0])
+    assert np.allclose(adata.layers["velocity"][0], bdata.layers["velocity"][0])
+    assert np.allclose(
+        adata.uns["velocity_graph"].data[:5], bdata.uns["velocity_graph"].data[:5]
+    )
